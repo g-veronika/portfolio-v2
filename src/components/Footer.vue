ed0cfd10ceb688af0fcf2f4c36f0f498ca0741e4
g-veronika <g.veronika2021@gmail.com>
2023-01-10 18:44:27 +0100

    add footer translation on hover

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,18 @@
 <template>
   <footer class="self-center pb-4">
     <div class="opacity-50">Designed & Built by Veronika</div>
+    <div
+      class="mail cursor-pointer right-0 fixed bottom-10 mr-4 text-h2 hover:text-secondary ease-in duration-200 hover:translate-y-[-3px]"
+    >
+      gveronikaa@hotmail.com
+    </div>
   </footer>
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.mail {
+  writing-mode: vertical-rl;
+}
+</style>
